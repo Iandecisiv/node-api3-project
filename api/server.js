8bcdc0732ca33a6317250cf81d422fc0b68dae6f
Ian-Tucker <iandecisiv@gmail.com>
2021-02-25 13:58:17 -0600

    MVP

--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
-const express = require('express');
-
+const express = require("express");
 const server = express();
+const logger = require('./middleware/logger');
+const welcomeRouter = require('./welcome.js');
+const userRouter = require('./users/users-router');
+const postRouter = require('./posts/posts-router');
 
+server.use(logger());
+
+server.use(express.json());
+
+server.use('/', welcomeRouter);
+server.use('/users', userRouter);
+server.use('/posts', postRouter);
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and routes need to be connected here
